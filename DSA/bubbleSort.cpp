4ca5947627629026bef6dbefb07f6f0bc0200c81
harjyot001 <harjyotsingh8055@gmail.com>
2025-09-18 15:28:13 +0000

    bubblesort

--- DSA/bubbleSort.cpp
+++ DSA/bubbleSort.cpp
@@ -1,6 +1,17 @@
 #include<iostream>
 using namespace std;
 
+int show(int arr[]){
+   
+  int i;
+  for(i=0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+}
+
 int main(){
-    cout<<"Hello";
+    int arr[5]={1,2,3,4,5};
+    show(arr,size);
+
+  
 }
