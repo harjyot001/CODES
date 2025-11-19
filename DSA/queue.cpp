e9242f1fb377bec20fbc8d3a3f6a4a5842c8f039
harjyot001 <harjyotsingh8055@gmail.com>
2025-11-19 15:20:50 +0000

    jj

--- DSA/queue.cpp
+++ DSA/queue.cpp
@@ -1,102 +1,65 @@
- #include<stdio.h>   
-#include<stdlib.h>  
-#define maxsize 5  
-void insert();  
-void delete();  
-void display();  
-int front = -1, rear = -1;  
-int queue[maxsize];  
-void main ()  
-{  
-    int choice;   
-    while(choice != 4)   
-    {     
-        printf("\n*************************Main Menu*****************************\n");  
-        printf("\n=================================================================\n");  
-        printf("\n1.insert an element\n2.Delete an element\n3.Display the queue\n4.Exit\n");  
-        printf("\nEnter your choice ?");  
-        scanf("%d",&choice);  
-        switch(choice)  
-        {  
-            case 1:  
-            insert();  
-            break;  
-            case 2:  
-            delete();  
-            break;  
-            case 3:  
-            display();  
-            break;  
-            case 4:  
-            exit(0);  
-            break;  
-            default:   
-            printf("\nEnter valid choice??\n");  
-        }  
-    }  
-}  
-void insert()  
-{  
-    int item;  
-    printf("\nEnter the element\n");  
-    scanf("\n%d",&item);      
-    if(rear == maxsize-1)  
-    {  
-        printf("\nOVERFLOW\n");  
-        return;  
-    }  
-    if(front == -1 && rear == -1)  
-    {  
-        front = 0;  
-        rear = 0;  
-    }  
-    else   
-    {  
-        rear = rear+1;  
-    }  
-    queue[rear] = item;  
-    printf("\nValue inserted ");  
-      
-}  
-void delete()  
-{  
-    int item;   
-    if (front == -1 || front > rear)  
-    {  
-        printf("\nUNDERFLOW\n");  
-        return;  
-              
-    }  
-    else  
-    {  
-        item = queue[front];  
-        if(front == rear)  
-        {  
-            front = -1;  
-            rear = -1 ;  
-        }  
-        else   
-        {  
-            front = front + 1;  
-        }  
-        printf("\nvalue deleted ");  
-    }  
-      
-      
-}  
-      
-void display()  
-{  
-    int i;  
-    if(rear == -1)  
-    {  
-        printf("\nEmpty queue\n");  
-    }  
-    else  
-    {   printf("\nprinting values .....\n");  
-        for(i=front;i<=rear;i++)  
-        {  
-            printf("\n%d\n",queue[i]);  
-        }     
-    }  
-}  
+#include<iostream>
+using namespace std;
+#define maxsize 5
+
+int queue[maxsize];
+int *front;
+int *rear;
+
+void insert(){
+    int val;
+    cout<<"Enter value";
+    cin>>val;
+
+    if(rear== maxsize-1 && rear>front) {
+        cout<<"Full\n";
+        return;
+    }else if(rear ==-1 && front == -1){
+        rear = 0;
+        front = 0;
+        queue[front] = val;
+        cout<<"inserted";
+    }else{
+        rear +=1;
+        queue[rear] = val;
+        cout<<"inserted";
+    } 
+
+}
+
+
+
+void delete(){
+    if(front>rear && front ==-1){
+        cout<<"empty";
+        return;
+    }else if (rear==0 && front ==0){
+        rear = -1;
+        front = -1;
+        cout<<"deleted";
+
+    }else{
+        front = front+1;
+        cout<<"deleted";
+    }
+}
+
+
+void display(){
+    int i;
+    for(i=front;i<=rear;i++){
+        cout<<queue[i]<<" ";
+    }
+}
+
+int main(){
+
+
+    insert(10);
+    insert(20);
+    insert(30);
+    display();
+
+}
+
+
