890b3ab096404779ddd20926d1b2064b87e05098
harjyot001 <harjyotsingh8055@gmail.com>
2025-12-08 13:39:17 +0000

    gg

--- DSA/linklistinsertion.cpp
+++ DSA/linklistinsertion.cpp
@@ -1,44 +1,80 @@
-#include<iostream>
-#include<stdlib.h>
+ #include <iostream>
+#include <cstdlib>
 using namespace std;
-struct node
-{
+
+struct node {
     int data;
-    struct node* next;
+    node* next;
 };
-struct node*head;
+
+node* head = nullptr;
 
 void beginsert();
-// void lastinsert();
-// void randominsert();
+void lastinsert();
+void randominsert();
+void begin_delete();
+void last_delete();
+void random_delete();
+void searchNode();
 void display();
 
-int main(){
-    beginsert();
-    // lastinsert();
-    display();
+int main() {
+    int choice = 0;
 
+    while (choice != 9) {
+        cout << "\n\n********* Main Menu *********\n";
+        cout << "1. Insert at beginning\n"
+             << "2. Insert at last\n"
+             << "3. Insert at specific location\n"
+             << "4. Delete from beginning\n"
+             << "5. Delete from last\n"
+             << "6. Delete from specific location\n"
+             << "7. Search for an element\n"
+             << "8. Display list\n"
+             << "9. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
 
+        switch (choice) {
+            case 1: beginsert(); break;
+            case 2: lastinsert(); break;
+            case 3: randominsert(); break;
+            case 4: begin_delete(); break;
+            case 5: last_delete(); break;
+            case 6: random_delete(); break;
+            case 7: searchNode(); break;
+            case 8: display(); break;
+            case 9: exit(0);
+            default: cout << "Please enter valid choice.\n";
+        }
+    }
+
+    return 0;
 }
-
-
-void beginsert(){
-    struct node*ptr;
+void begininsert(){
     int item;
-    ptr=(struct node*) malloc(sizeof(struct node*));
-    cout<<"enter the value"<<endl;
+    node*ptr = new node();
+    cout<<"enter value:";
     cin>>item;
     ptr->data=item;
     ptr->next=head;
     head=ptr;
+    cout<<"node inserted at beginning\n";
 }
+void lastinsert(){
+    int item;
+    node*ptr = new node();
+    cout<<"enter value:";
+    cin>>item;
+    ptr->data = item;
+    ptr->next = nullptr;
+    if (head == nullptr) {
+        head = ptr;
+    } else {
+        node* temp = head;
+        while (temp->next != nullptr) {
+            temp = temp->next; 
 
- void display(){
-     struct node*ptr;
-        ptr=head;
-        while(ptr!=NULL){
-            cout<<ptr->data<<endl;
-            ptr=ptr->next;
+            temp->
 
-        }
 }
